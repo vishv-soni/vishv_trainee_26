d8981b9871206038805e5f13677f03164486d8b3
Vishv Soni <vishv.lavingiya@icreativetechnolabs.com>
2026-01-13 15:50:27 +0530

    adminLte added

--- php/advance/cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-setcookie("username". "vishv", time() + (86400*30), "/");
-
-if(isset($_COOKIE["username"])){
-    echo "welcome";
-}
--- php/crud/delete.php
+++
@@ -1,7 +0,0 @@
-​<?php
-
-include 'db.php';
-$id = $_GET['id'];
-
-mysqli_query($conn, "DELETE FROM users WHERE id=$id");
-header("Location: view.php");
